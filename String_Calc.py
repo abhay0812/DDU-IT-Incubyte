777e7618f24141fdf2761b8a28313adb68d6901e
abhay0812 <80241301+abhay0812@users.noreply.github.com>
2021-10-23 10:36:21 +0530

    Complete Test Case 1,2,5
    Complete Test Case 1,2,5

--- String_Calc.py
+++ String_Calc.py
@@ -1,11 +1,20 @@
 s = input("Enter string:- ")
 l = list(s.split(','))
 
+negative=[]
+
 if len(l)==1 and l[0]=="":
     print(0)
 else:
     sum = 0
     for num in l:
-        sum+=int(num)
-        
-    print(sum)
+        if int(num)<0:
+            negative.append(int(num))
+        else:
+            sum+=int(num)
+    
+    if len(negative)>0:
+        print("negatives not allowed:-",end=" ")
+        print(negative)
+    else:
+        print(sum)
